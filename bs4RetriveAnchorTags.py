5db9f830059784c12a2a2fa55ec4bc08f8c383e5
shankarswamy <raknahs@yahoo.com>
2017-03-03 15:42:01 -0800

    First commit
    Retrieve anchor flags from a website.

---
+++ bs4RetriveAnchorTags.py
@@ -0,0 +1,16 @@
+import urllib.request
+from bs4 import *
+
+url = input('Enter:  ')
+html = urllib.request.urlopen(url).read()
+
+soup = BeautifulSoup(html, 'html.parser')
+
+# Retrieve all of the anchor tags
+tags = soup('a')
+for tag in tags:
+    # Look at the parts of a tag
+    print('TAG:',tag)
+    print('URL:',tag.get('href', None))
+    print('Contents:',tag.contents[0])
+    print('Attrs:',tag.attrs)
